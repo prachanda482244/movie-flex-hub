15b3e1fd701954277decc28cc266ff7f564e1e3f
Prachanda Rana <62231260+prachanda482244@users.noreply.github.com>
2023-08-16 21:22:02 +0545

    Feat make the design of the movie website

--- movieflixhub/src/Components/Movies.js
+++ movieflixhub/src/Components/Movies.js
@@ -1,14 +1,26 @@
 import React from 'react'
 import { useGlobalContext } from '../Hooks/GlobalHooks'
-
+import { NavLink } from 'react-router-dom'
 const Movies = () => {
     const { movies } = useGlobalContext()
     return (
-        <div>
-            {movies?.map((movie, id) => {
-                return <div key={id}>{movie.Title}</div>
-            })}
-        </div>
+        <section className='movie-page'>
+            <div className='grid lg:grid-cols-4 md:grid-cols-2 gap-4 py-2'>
+
+                {movies?.map((movie, id) => {
+                    const { imdbID, Title, Poster } = movie
+                    return <NavLink to={`/movie/${imdbID}`} key={imdbID}>
+                        <div className='card'>
+                            <div className='card-info'>
+                                <h2 className='text-center '>{Title.length <= 20 ? Title : Title.slice(0, 20)}</h2>
+                                <img src={Poster} alt="Poster" />
+                            </div>
+
+                        </div>
+                    </NavLink>
+                })}
+            </div>
+        </section>
     )
 }
 
